75951a8cab5c09473cd593c53e4a6b2e1f2a4eff
Hardik Singh <91072109+Hardik-Singh04@users.noreply.github.com>
2022-02-08 12:22:24 +0530

    Update code.kt

--- Returning Rolled Dice Values using objects and classes/code.kt
+++ Returning Rolled Dice Values using objects and classes/code.kt
@@ -1,1 +1,27 @@
+fun main() 
+{
+    val myfirstdice=Dice(6)
+    val dice1=myfirstdice.roll()
+    println("Your ${myfirstdice.Sides} sided dice rolled ${dice1}!")
+    
+    val myseconddice = Dice(20)
+    val dice2=myseconddice.roll()
+    println("Your ${myseconddice.Sides} sided dice rolled ${dice2}!")
+    
+    val mythirddice = Dice(40)
+    val dice3 = mythirddice.roll()
+    println("Your ${mythirddice.Sides} sided dice rolled ${dice3}!")
+    
+    val myfourthdice = Dice(60)
+    val dice4 = myfourthdice.roll()
+    println("Your ${myfourthdice.Sides} sided dice rolled ${dice4}!")
+}
 
+class Dice (val Sides: Int) 
+{
+    fun roll(): Int 
+    {
+        val randomnumber = (1..Sides).random()
+        return randomnumber
+    }
+}
