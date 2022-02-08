7213d05f9d8ae391f9dc48eaad734d7ce94d6519
Hardik Singh <91072109+Hardik-Singh04@users.noreply.github.com>
2022-02-08 12:07:21 +0530

    Update code.kt

--- Rolling dice using objects and classes/code.kt
+++ Rolling dice using objects and classes/code.kt
@@ -1,1 +1,24 @@
+fun main() 
+{
+  val myfirstdice=dice()
+  val myseconddice=dice()
+  val mythirddice=dice()
+  
+  val dice1=myfirstdice.roll()
+  val dice2=myseconddice.roll()
+  val dice3=mythirddice.roll()
+  
+  println("Your ${myfirstdice.sides} sided dice rolled ${dice1}!")
+  println("Your ${myfirstdice.sides} sided dice rolled ${dice2}!")
+  println("Your ${myfirstdice.sides} sided dice rolled ${dice3}!")
+}
 
+class dice
+{
+    var sides=6
+    fun roll():Int
+    {
+        val randomnumber=(1..6).random()
+        return randomnumber
+    }   
+}
